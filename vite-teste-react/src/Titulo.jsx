5e743cfcde5dcb039084245b96bd59730e3af214
Natan Rubenich <57855715+NatanRubenich@users.noreply.github.com>
2025-05-27 08:46:49 -0300

    Update Titulo.jsx
    Mudanças no titulo

--- vite-teste-react/src/Titulo.jsx
+++ vite-teste-react/src/Titulo.jsx
@@ -1,35 +1,3 @@
-/*
-PRIMEIRO ASSIM:
-function Titulo() {
-  return (
-    <div>
-        <h1>Oi, eu sou um título</h1>
-        <p>lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.</p>
-    </div>
-  )
-}
-export default Titulo;
-*/
-
-
-/* 
-SEGUNDAMENTE FAÇA ASSIM: 
-function Titulo() {
-
-    let nome = "Natan";
-    //const urlImagem = "https://down-br.img.susercontent.com/file/br-11134207-7r98o-m8kgvn1yhzwx96";
-    return (
-    <div>
-        <h1>Oi, eu sou {nome}</h1>
-        {/*<img src={urlImagem} /> /}
-        <p>lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.</p>
-    </div>
-  )
-}
-export default Titulo;
-*/
-
-
 function Titulo( {nome} ) {
  return (
     <div>
